99f68a37fa937b1ce175c61b66989b9a54984e19
ivan <ivan.rodriguez.o@gmail.com>
2015-11-16 15:53:23 -0600

    refactored file structure, added readme endpoints

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # wazzinga
 
-Platform for sending anonymous Whatsapp messages through a network of client nodes
+App for sending anonymous Whatsapp messages using a list of client nodes
 
+API endpoints
+
+- /v1/code
+- /v1/register
+- /v1/send
+- /v1/nodes
--- api.php
+++ api.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require './vendor/autoload.php';
 require_once './vendor/whatsapp/chat-api/src/Registration.php';
 require_once './vendor/whatsapp/chat-api/src/whatsprot.class.php';
 
@@ -7,7 +7,7 @@ $app = new \Slim\Slim();
 $app->response->headers->set('Content-Type', 'application/json');
 
 // Get code by SMS
-$app->get('/api/v1/code', function () use ($app) {
+$app->get('/v1/code', function () use ($app) {
     $username = $app->request()->get('number');
     $debug = false;
     $w = new Registration($username, $debug);
@@ -15,14 +15,14 @@ $app->get('/api/v1/code', function () use ($app) {
 });
 
 // Register code 
-$app->get('/api/v1/register', function () use ($app) {
+$app->get('/v1/register', function () use ($app) {
     $username = $app->request()->get('number');
     $debug = false;
     $w = new Registration($username, $debug);
     echo json_encode($w->codeRegister('sms'));
 });
 // Send message
-$app->get('/api/v1/send', function () use ($app) {
+$app->get('/v1/send', function () use ($app) {
     $nodes = _readClientNodes();
     $node = $nodes[array_rand($nodes)];
     $sender = $node['number'];
@@ -36,7 +36,7 @@ $app->get('/api/v1/send', function () use ($app) {
     $w->sendMessage($destination, $message);
 });
 // List client nodes
-$app->get('/api/v1/nodes', function () use ($app) {
+$app->get('/v1/nodes', function () use ($app) {
     $nodes = _readClientNodes();
     echo json_encode($nodes);
 });
@@ -44,6 +44,6 @@ $app->get('/api/v1/nodes', function () use ($app) {
 $app->run();
 
 function _readClientNodes() {
-  $nodes = include('../nodes.php');
+  $nodes = include('./nodes.php');
   return $nodes;
 }
